a8cf3a5fd01aa14ea02f5606961b16ee0715b317
abeshi03 <k_913411@yahoo.co.jp>
2021-10-10 22:29:41 +0900

    routing.tsを追加

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,11 @@
 // - フレームワーク =======================================================================================================
 import React, { VFC } from "react";
+import Head from "next/head";
+
+// - ルーティング ========================================================================================================
+import Routing from "../routing/routing";
+
+// - 子コンポーネント =====================================================================================================
 import { Breadcrumb, BreadcrumbLink } from "../components/atoms/Breadcrumb/Breadcrumb";
 import { Button } from "../components/atoms/Button/Button";
 
@@ -7,22 +13,26 @@ import { Button } from "../components/atoms/Button/Button";
 const links: BreadcrumbLink[] = [
   {
     label: "ホーム",
-    path: "/"
+    path: Routing.Top.URI_Path
   },
   {
     label: "記事一覧"
   },
-]
+];
 // =====================================================================================================================
 
 const Home: VFC = () => {
   return(
     <>
+      <Head>
+        <title>{Routing.Top.pageName}</title>
+      </Head>
       <Breadcrumb
         // 表示テスト後ほどページから削除
         // @ts-ignore
         links={links}
       />
+      {/*表示テスト後ほどページから削除*/}
       <Button color={"SKY_BLUE"} path={"/"} size={"BIG"}>テストボタン</Button>
       <Button color={"WHITE"} path={"/"} size={"SMALL"}>テストボタン</Button>
     </>
---
+++ src/routing/routing.ts
@@ -0,0 +1,9 @@
+namespace Routing {
+  // - トップ ===========================================================================================================
+  export namespace Top {
+    export const URI_Path: string = "/";
+    export const pageName: string = "トップ";
+  }
+}
+
+export default Routing;
