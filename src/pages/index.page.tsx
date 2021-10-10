16036ba74a53da883bd41c4a3fd9a33f76820533
abeshi03 <k_913411@yahoo.co.jp>
2021-10-10 21:19:57 +0900

    Buttonコンポーネントを追加

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 // - フレームワーク =======================================================================================================
 import React, { VFC } from "react";
 import { Breadcrumb, BreadcrumbLink } from "../components/atoms/Breadcrumb/Breadcrumb";
+import { Button } from "../components/atoms/Button/Button";
 
 // 表示テスト後ほど削除 ====================================================================================================
 const links: BreadcrumbLink[] = [
@@ -16,11 +17,15 @@ const links: BreadcrumbLink[] = [
 
 const Home: VFC = () => {
   return(
-    <Breadcrumb
-      // 表示テスト後ほどページから削除
-      // @ts-ignore
-      links={links}
-    />
+    <>
+      <Breadcrumb
+        // 表示テスト後ほどページから削除
+        // @ts-ignore
+        links={links}
+      />
+      <Button color={"SKY_BLUE"} path={"/"} size={"BIG"}>テストボタン</Button>
+      <Button color={"WHITE"} path={"/"} size={"SMALL"}>テストボタン</Button>
+    </>
   );
 };
 
