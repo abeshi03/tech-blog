001ac3afd1bd3e9d9f1ec7cc18a214eb71bed2c0
abeshi03 <k_913411@yahoo.co.jp>
2021-10-24 00:12:18 +0900

    プロフィールのgetの非同期処理を分離

---
+++ src/apis/ProfileAPI.ts
@@ -0,0 +1,31 @@
+// - ライブラリー ========================================================================================================
+import axios from "axios";
+
+// - 型定義 =============================================================================================================
+import { Profile } from "../types/profile/profile";
+
+// - state =============================================================================================================
+import { initMyProfileState } from "../constants/initState";
+
+
+export const getMyProfile = async (): Promise<Profile> => {
+
+  let myProfile: Profile = initMyProfileState;
+
+  try {
+
+    const baseEndPoint: string = process.env.NEXT_PUBLIC_ENDPOINT;
+
+    const response = await axios.get<Profile>(`${baseEndPoint}my_profile`,{
+      headers: { "X-API-KEY": process.env.NEXT_PUBLIC_PROFILE_API_KEY }
+    })
+
+    myProfile = response.data;
+
+  } catch (error: unknown) {
+
+    throw new Error("getStaticProps_error: myProfile");
+  }
+
+  return myProfile;
+}
---
+++ src/constants/initState.ts
@@ -0,0 +1,11 @@
+// - 型定義 =============================================================================================================
+import { Profile } from "../types/profile/profile";
+
+export const initMyProfileState: Profile = {
+  familyName: "",
+  lastName: "",
+  role: "",
+  image: {
+    url: ""
+  }
+}
