e95dbdf13882e2ca88e2abf40241571e728eb6aa
abeshi03 <k_913411@yahoo.co.jp>
2021-10-23 23:07:10 +0900

    headerファイル作成, topPage初期化

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -13,26 +13,6 @@ import { Profile } from "../types/profile/profile";
 // - ルーティング ========================================================================================================
 import { Routing } from "../routing/routing";
 
-// - 子コンポーネント =====================================================================================================
-import { Breadcrumb, BreadcrumbLink } from "../components/atoms/Breadcrumb/Breadcrumb";
-import { Button } from "../components/atoms/Button/Button";
-import { IconAndLink } from "../components/atoms/IconAndLink/IconAndLink";
-import { ProfileCard } from "../components/organisms/Cards/ProfileCard";
-import { AlertBox } from "../components/atoms/AlertBox/AlertBox";
-import { Footer } from "../components/layouts/Footer/Footer";
-
-// 表示テスト後ほど削除 ====================================================================================================
-const links: BreadcrumbLink[] = [
-  {
-    label: "ホーム",
-    path: Routing.Top.path
-  },
-  {
-    label: "記事一覧"
-  },
-];
-// =====================================================================================================================
-
 type Props = {
   myProfile: Profile
 }
@@ -45,24 +25,7 @@ const Home: VFC<Props> = ({ myProfile }) => {
       <Head>
         <title>{Routing.Top.pageName}</title>
       </Head>
-      <Breadcrumb
-        links={links}
-      />
-      {/*表示テスト後ほどページから削除*/}
-      <Button color={"SKY_BLUE"} path={"/"} size={"BIG"}>テストボタン</Button>
-      {/*表示テスト後ほどページから削除*/}
-      <Button color={"WHITE"} path={"/"} size={"SMALL"}>テストボタン</Button>
-      {/*表示テスト後ほどページから削除*/}
-      <IconAndLink iconType="GITHUB" label="github_id" externalLink={ExternalLinks.Github}/>
-      {/*表示テスト後ほどページから削除*/}
-      <ProfileCard
-        targetProfile={myProfile}
-      />
-      <AlertBox title="エラーテスト" description="エラーテスト" alertType="ERROR" />
-      <AlertBox title="お知らせテスト" description="お知らせテスト" alertType="INFO" />
-      <AlertBox title="警告テスト" description="警告テスト" alertType="WARNING" />
-      <AlertBox title="成功テスト" description="成功テスト" alertType="SUCCESS" />
-      <Footer/>
+      <h1>実装待機</h1>
     </>
   );
 };
