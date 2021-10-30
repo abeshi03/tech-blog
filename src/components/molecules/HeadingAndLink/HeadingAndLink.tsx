1a0973f14da510932b90bab1c1cd4a02ef9f825b
abeshi03 <k_913411@yahoo.co.jp>
2021-10-30 13:26:18 +0900

    不要なカッコの修正

--- src/components/molecules/HeadingAndLink/HeadingAndLink.tsx
+++ src/components/molecules/HeadingAndLink/HeadingAndLink.tsx
@@ -16,10 +16,10 @@ type Props = {
 /* eslint-disable-next-line react/display-name */
 export const HeadingAndLink: VFC<Props> = memo((props) => {
   const { heading, linkName, path, style } = props;
-  
+
 return (
     <div className={styles.headingAndLink} style={style}>
-      <h1 className={"heading2"}>{ heading }</h1>
+      <h1 className="heading2">{ heading }</h1>
       <Link href={path}>
         <a className={styles.linkName}>{ `${linkName} >` }</a>
       </Link>
