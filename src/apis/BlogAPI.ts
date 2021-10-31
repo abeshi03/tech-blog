9d62ecb21ef3e539c9bb8a07caea1e199d458c17
abeshi03 <k_913411@yahoo.co.jp>
2021-11-01 08:33:57 +0900

    BlogAPI.tsの処理をリファクタリング

--- src/apis/BlogAPI.ts
+++ src/apis/BlogAPI.ts
@@ -3,28 +3,31 @@ import axios from "axios";
 
 // - 型定義 =============================================================================================================
 import { BlogResponseData } from "../types/Profile/Blog/Blog";
-import { initBlogData } from "../constants/initState";
 
+// - 定数 ===============================================================================================================
+import { BASE_END_POINT } from "../constants/apis";
+import { X_API_KEY } from "../constants/apis";
 
-export const getBlogs = async (): Promise<BlogResponseData> => {
+export async function getBlogs(): Promise<BlogResponseData> {
 
-  let blogData: BlogResponseData = initBlogData;
 
   try {
 
-    const baseEndPoint: string = process.env.NEXT_PUBLIC_ENDPOINT;
-
-    const response: BlogResponseData = await axios.get(`${baseEndPoint}blog`, {
-      headers: { "X-API-KEY": process.env.NEXT_PUBLIC_PROFILE_API_KEY }
+    const response: BlogResponseData = await axios.get(`${BASE_END_POINT}blog`, {
+      headers: { "X-API-KEY": X_API_KEY }
     });
 
-    blogData.data.contents = response.data.contents;
-    blogData.data.totalCount = response.data.totalCount;
+    const blogData: BlogResponseData = {
+      data: {
+        contents: response.data.contents,
+        totalCount: response.data.totalCount
+      }
+    }
+
+    return blogData;
 
   } catch (error: unknown) {
     console.log(error);
     throw new Error("API ERROR: getBlogs");
   }
-
-  return blogData;
-};
+}
