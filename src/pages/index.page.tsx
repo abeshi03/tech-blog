08aae420158247c72418b8e430649db67da5707c
abeshi03 <k_913411@yahoo.co.jp>
2021-11-02 09:17:12 +0900

    トップページに新着記事3件を表示

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -20,6 +20,7 @@ import { getBlogs } from "../apis/BlogAPI";
 // - 子コンポーネント =====================================================================================================
 import { BlogCard } from "../components/organisms/Cards/BlogCard/BlogCard";
 import {ProfileCard} from "../components/organisms/Cards/ProfileCard/ProfileCard";
+import { HeadingAndLink } from "../components/molecules/HeadingAndLink/HeadingAndLink";
 
 type Props = {
   myProfile: Profile;
@@ -37,8 +38,22 @@ const Home: VFC<Props> = (props) => {
       </Head>
       <main className={styles.topPage}>
         <div className={styles.mainSection}>
+
+          {/*　ブログ記事 ============================================================================================= */}
           <div className={styles.blogsBlock}>
+            <HeadingAndLink
+              heading="新着記事"
+              linkName="記事一覧"
+              path="#"
+            />
+            <div className={styles.blogsCardFlow}>
+              {blogs.data.contents.map((blog: Blog) => (
+                <BlogCard key={blog.id} targetBlog={blog}/>
+              ))}
+            </div>
           </div>
+
+          {/*プロフィール ============================================================================================= */}
           <div className={styles.profileBlock}>
             <ProfileCard targetProfile={myProfile}/>
           </div>
@@ -54,7 +69,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const [ myProfile, blogs ]: [ Profile, BlogResponseData ] = await Promise.all([
     getMyProfile(),
-    getBlogs()
+    getBlogs(3)
   ]);
 
   return {
