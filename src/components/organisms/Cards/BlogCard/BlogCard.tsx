30fce500f266e12a7d533bf769f8ab6628e7dee2
abeshi03 <k_913411@yahoo.co.jp>
2021-11-03 17:19:08 +0900

    ブログカード: 記事詳細への導線を追加

--- src/components/organisms/Cards/BlogCard/BlogCard.tsx
+++ src/components/organisms/Cards/BlogCard/BlogCard.tsx
@@ -1,5 +1,6 @@
 // - フレームワーク =======================================================================================================
 import React, { memo, VFC } from "react";
+import { useRouter } from "next/router";
 
 // - アセット ============================================================================================================
 import styles from "./blogCard.module.scss";
@@ -7,16 +8,17 @@ import styles from "./blogCard.module.scss";
 // - 型定義 =============================================================================================================
 import { Blog } from "../../../../types/Profile/Blog/Blog";
 import { Category } from "../../../../types/Category";
+import {Routing} from "../../../../routing/routing";
 
 type Props = {
   style?: React.CSSProperties;
-  targetBlog: Pick<Blog, "mainImage" | "categories" | "createdAt" | "title">;
+  targetBlog: Pick<Blog, "id" | "mainImage" | "categories" | "createdAt" | "title">;
 }
 
 const formattedCreatedAt = (targetDate: string): string => {
 
   const date: Date = new Date(targetDate);
-  
+
 return `${date.getFullYear()}/` +
     `${date.getMonth() + 1}/`　+
     `${date.getDate()}`;
@@ -24,10 +26,16 @@ return `${date.getFullYear()}/` +
 
 /* eslint-disable-next-line react/display-name */
 export const BlogCard: VFC<Props> = memo((props) => {
+
   const { style, targetBlog } = props;
+  const router = useRouter();
+
+  const onClickBlogDetailsPage = (): void => {
+    router.push(Routing.Blog.Details.path.replace("[id]", targetBlog.id));
+  }
 
 return (
-    <div className={styles.blogCard} style={style}>
+    <div className={styles.blogCard} style={style} onClick={onClickBlogDetailsPage}>
       <div className={styles.image} style={{backgroundImage: `url(${targetBlog.mainImage.url})`}} role="img"></div>
 
       <div className={styles.categoriesFlow}>
