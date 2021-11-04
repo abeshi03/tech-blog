c8a3aa8dbf31f1962f0528bbf66918e037b092bf
abeshi03 <k_913411@yahoo.co.jp>
2021-11-05 00:19:53 +0900

    記事詳細: タイトル, カテゴリー, Dateを表示

--- src/pages/blog/[id]/index.page.tsx
+++ src/pages/blog/[id]/index.page.tsx
@@ -1,5 +1,6 @@
 // - フレームワーク, ライブラリー ===========================================================================================
 import React, { VFC } from "react";
+import Link from "next/link";
 import { GetStaticPaths, GetStaticProps } from "next";
 
 // - アセット ===========================================================================================================
@@ -13,12 +14,23 @@ import { getBlogData, getBlogs } from "../../../apis/BlogAPI";
 
 // - 型定義 =============================================================================================================
 import { Blog, BlogResponseData } from "../../../types/Profile/Blog/Blog";
+import { Category } from "../../../types/Category";
 
 // - 子コンポーネント =====================================================================================================
 import { Breadcrumb, BreadcrumbLink } from "../../../components/atoms/Breadcrumb/Breadcrumb";
+import {CategoryBadge} from "../../../components/atoms/CategoryBadge/CategoryBadge";
 
 type Props = {
   blog: Blog
+}
+
+const formattedPublishedDate = (targetDate: string): string => {
+
+  const publishedDate: Date = new Date(targetDate);
+
+  return `${ publishedDate.getFullYear()}年` +
+          `${ publishedDate.getMonth() + 1}月` +
+          `${ publishedDate.getDate()}日`;
 }
 
 const BlogDetails: VFC<Props> = (props) => {
@@ -40,9 +52,28 @@ const BlogDetails: VFC<Props> = (props) => {
   ]
 
   return (
-    <div className={styles.blogDetailsPage}>
+    <main className={styles.blogDetailsPage}>
       <Breadcrumb links={breadcrumbLinks}/>
-    </div>
+      <div className={styles.mainSection}>
+
+        {/* ブログ記事 ===============================================================================================　*/}
+        <div className={styles.blogBlock}>
+
+          <h1 className="heading1">{ blog.title }</h1>
+
+          <div className={styles.publishedDate}>{ formattedPublishedDate(blog.publishedAt) }</div>
+
+          <div className={styles.categoriesFlow}>
+            {blog.categories.map((category: Category) => (
+              <CategoryBadge category={category}/>
+            ))}
+          </div>
+        </div>
+
+        {/* サイドバー ===============================================================================================　*/}
+        <div className={styles.sideBar}></div>
+      </div>
+    </main>
   );
 };
 
