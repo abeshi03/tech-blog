c18d54062097acb781198e881cf1f4c41bf1f6eb
abeshi03 <k_913411@yahoo.co.jp>
2021-11-06 23:14:42 +0900

    DisplaySwitchingButtonコンポーネントを追加

--- src/assets/icons/PlusIcon.tsx
+++ src/assets/icons/PlusIcon.tsx
@@ -4,11 +4,10 @@ import React, { memo, VFC } from "react";
 /* eslint-disable-next-line react/display-name */
 export const PlusIcon: VFC = memo(() => {
   return (
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 25 25">
-      <g id="Group_320" transform="translate(-1201 -399)">
-        <circle id="Ellipse_1" cx="1213.5" cy="411.5" r="12.5"/>
-        <path className="st0" d="M1213.4 406.4h1v10.2h-1z"/>
-        <path className="st0" d="M1208.8 411h10.2v1h-10.2z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" xmlSpace="preserve">
+      <g transform="translate(-.3)">
+        <circle cx="10.3" cy="10" r="10"/>
+        <path d="m11.12 14.2-.07-3.46h3.4c.42 0 .84-.42.84-.84a.91.91 0 0 0-.85-.85h-3.4v-3.4a.91.91 0 0 0-.84-.85.91.91 0 0 0-.85.85v3.4h-3.4a.91.91 0 0 0-.84.85c0 .42.42.84.85.84h3.4v3.4c0 .42.42.85.84.85.5.07.85-.29.92-.78z" fill="#fff"/>
       </g>
     </svg>
   );
---
+++ src/components/atoms/DisplaySwitchingButton/DisplaySwitchingButton.tsx
@@ -0,0 +1,52 @@
+// - フレームワーク =======================================================================================================
+import React, { memo, ReactNode, useState, VFC } from "react";
+
+// - アセット ============================================================================================================
+import styles from "./displaySwitchingButton.module.scss";
+import { PlusIcon } from "../../../assets/icons/PlusIcon";
+import { MinusIcon } from "../../../assets/icons/MinusIcon";
+
+
+type Props = {
+  label: string;
+  children: ReactNode;
+  style?: React.CSSProperties;
+}
+
+/* eslint-disable-next-line react/display-name */
+export const DisplaySwitchingButton: VFC<Props> = memo((props) => {
+
+  const { label, children, style } = props;
+
+  const [ displayChildren, setDisplayChildren ] = useState<boolean>(true);
+
+  const onClickDisplayChildren = (): void => {
+    setDisplayChildren(!displayChildren);
+  }
+
+  return (
+    <div className={styles.displaySwitchingButton} onClick={onClickDisplayChildren} style={style}>
+
+      { displayChildren ? (
+        <div className={styles.flexContainer}>
+          <div className={styles.label}>{ `${label}を非表示` }</div>
+          <div className={styles.icon}>
+            <MinusIcon/>
+          </div>
+        </div>
+
+      ) : (
+
+        <div className={styles.flexContainer}>
+          <div className={styles.label}>{ `${label}を表示` }</div>
+          <div className={styles.icon}>
+            <PlusIcon/>
+          </div>
+        </div>
+
+      )}
+
+      { displayChildren && <div>{ children }</div> }
+    </div>
+  );
+});
