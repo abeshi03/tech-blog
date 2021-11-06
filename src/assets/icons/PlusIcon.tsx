931150d61e972f0dcb43ab9a20f198c0d4f92ef6
abeshi03 <k_913411@yahoo.co.jp>
2021-11-06 22:32:17 +0900

    プラスとマイナスのアイコン追加

---
+++ src/assets/icons/PlusIcon.tsx
@@ -0,0 +1,15 @@
+// - フレームワーク =======================================================================================================
+import React, { memo, VFC } from "react";
+
+/* eslint-disable-next-line react/display-name */
+export const PlusIcon: VFC = memo(() => {
+  return (
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 25 25">
+      <g id="Group_320" transform="translate(-1201 -399)">
+        <circle id="Ellipse_1" cx="1213.5" cy="411.5" r="12.5"/>
+        <path className="st0" d="M1213.4 406.4h1v10.2h-1z"/>
+        <path className="st0" d="M1208.8 411h10.2v1h-10.2z"/>
+      </g>
+    </svg>
+  );
+});
