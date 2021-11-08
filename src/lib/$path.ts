4d2847a2e3a0b2e845d9a9732bf6a7647e71b6ed
abeshi03 <k_913411@yahoo.co.jp>
2021-11-09 01:07:21 +0900

    ブログ一覧: ファイル設定完了

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -10,7 +10,8 @@ export const pagesPath = {
   blog: {
     _id: (id: string | number) => ({
       $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
-    })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/blog' as const, hash: url?.hash })
   },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
---
+++ src/pages/blog/index.page.tsx
@@ -0,0 +1,33 @@
+// - フレームワーク, ライブラリー ===========================================================================================
+import React, { VFC } from "react";
+
+// - アセット ============================================================================================================
+import styles from "./blogsListPage.module.scss";
+
+// - 子コンポーネント =====================================================================================================
+import {Breadcrumb, BreadcrumbLink} from "../../components/atoms/Breadcrumb/Breadcrumb";
+
+// - ルーティング ========================================================================================================
+import { pagesPath } from "../../lib/$path";
+import { Routing } from "../../routing/routing";
+
+const BlogsListPage: VFC = (props) => {
+
+  const breadcrumbLinks: BreadcrumbLink[] = [
+    {
+      path: pagesPath.$url().pathname,
+      label: Routing.Top.pageName
+    },
+    {
+      label: Routing.Blog.List.pageName
+    }
+  ]
+
+  return (
+    <div className={styles.blogsListPage}>
+      <Breadcrumb links={breadcrumbLinks}/>
+    </div>
+  )
+}
+
+export default BlogsListPage;
