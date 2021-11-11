9d3b227902e3eb8bdef608376e42783680d50ba8
abeshi03 <k_913411@yahoo.co.jp>
2021-11-12 00:10:45 +0900

    ページネーション: アルゴリズム実装完了

--- src/components/atoms/Pagination/Pagination.tsx
+++ src/components/atoms/Pagination/Pagination.tsx
@@ -10,31 +10,89 @@ import { pagesPath } from "../../../lib/$path";
 
 type Props = {
   totalCount: number;
+  currentPageNumber: number;
   perPageNumber: number;
 }
 
 /* eslint-disable-next-line react/display-name */
 export const Pagination: VFC<Props> = memo((props) => {
 
-  const { totalCount, perPageNumber } = props;
+  const { totalCount, perPageNumber, currentPageNumber } = props;
 
   const range = (start: number, end: number): number[] =>
     [...Array(end - start + 1)].map((_, i) => start + i);
 
+  const NUMBERED_BUTTONS_COUNT__MUST_BE_ODD: number = 2;
+
+  const mustDisplayGoToFirstPageButton = (): boolean => {
+    return currentPageNumber - (0.5 * (NUMBERED_BUTTONS_COUNT__MUST_BE_ODD - 1)) > 1;
+  }
+
+  const mustDisplayGoToLastPageButton = (): boolean => {
+    return currentPageNumber < totalPageCount - (0.5 * (NUMBERED_BUTTONS_COUNT__MUST_BE_ODD - 1));
+  }
+
+  const totalPageCount = Math.ceil(totalCount / perPageNumber);
+
   return (
     <>
-      {totalCount > perPageNumber &&
+      { totalPageCount > 1 &&
+        <nav className={styles.pagination}>
 
-        <ul className={styles.pagination}>
-          {range(1, Math.ceil(totalCount / perPageNumber)).map((pageID: number, index: number) => (
-            <li key={index}>
-              <Link href={pagesPath.blog.page._id(pageID).$url()}>
-                <a>{pageID}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+          { mustDisplayGoToFirstPageButton() &&
+            <Link href={pagesPath.blog.page._id(1).$url()}>
+              <a role="button">{ '最初<<' }</a>
+            </Link>
+          }
+
+          { currentPageNumber > 1 &&
+            <Link href={pagesPath.blog.page._id(currentPageNumber - 1).$url()}>
+              <a role="button">{ '前<' }</a>
+            </Link>
+          }
+
+          { currentPageNumber > 2 &&
+            <Link href={pagesPath.blog.page._id(currentPageNumber - 2).$url()}>
+              <a role="button">{ currentPageNumber - 2 }</a>
+            </Link>
+          }
+
+          { currentPageNumber > 1 &&
+            <Link href={pagesPath.blog.page._id(currentPageNumber - 1).$url()}>
+              <a role="button">{ currentPageNumber - 1 }</a>
+            </Link>
+          }
+
+          <span className={styles.currentPageNumber}>{ currentPageNumber }</span>
+
+          { currentPageNumber + 1 <= totalPageCount &&
+            <Link href={pagesPath.blog.page._id(currentPageNumber + 1).$url()}>
+              <a role="button">{ currentPageNumber + 1 }</a>
+            </Link>
+          }
+
+          { currentPageNumber + 2 <= totalPageCount &&
+            <Link href={pagesPath.blog.page._id(currentPageNumber + 2).$url()}>
+              <a role="button">{ currentPageNumber + 2 }</a>
+            </Link>
+          }
+
+          { currentPageNumber !== totalPageCount &&
+            <Link href={pagesPath.blog.page._id(currentPageNumber + 1).$url()}>
+              <a role="button">{ '>次' }</a>
+            </Link>
+          }
+
+          { mustDisplayGoToLastPageButton() &&
+            <Link href={pagesPath.blog.page._id(totalPageCount).$url()}>
+              <a role="button">{ '>>最後' }</a>
+            </Link>
+          }
+
+        </nav>
       }
     </>
   );
 });
+
+
