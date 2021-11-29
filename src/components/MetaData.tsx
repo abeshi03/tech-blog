497f2c0a1cfef0652dea84b663b58ed33c2ce2a7
abeshi03 <k_913411@yahoo.co.jp>
2021-11-29 20:50:24 +0900

    メタデータコンポーネント作成

---
+++ src/components/MetaData.tsx
@@ -0,0 +1,49 @@
+// - フレームワーク =======================================================================================================
+import React, {memo, VFC} from "react";
+import Head from "next/head";
+
+type OgType = "website" | "article"
+
+type TwitterCardType = "summary" | "summary_large_image"
+
+type Props = {
+  title: string;
+  description?: string;
+  noDefaultDescription: boolean;
+  ogpImageURI?: string;
+  url: string;
+  type: OgType;
+  twitterCardType: TwitterCardType
+}
+
+/* eslint-disable-next-line react/display-name */
+export const MetaData: VFC<Props> = memo((props) => {
+  const { title, description, ogpImageURI, url, type, twitterCardType, noDefaultDescription = false } = props;
+
+  const DEFAULT_TITLE = " | abeshi blog"
+  const DEFAULT_DESCRIPTION = "abeshi blogは、エンジニアの技術発信ブログサービスです。現役フロントエンドエンジニア目線からの最新技術の" +
+    "発信や、　情報を幅広く学ぶことができます。"
+
+  const metaTitle = `${title}${DEFAULT_TITLE}`;
+  const formattedDescription = description ? `${description}${DEFAULT_DESCRIPTION}` : DEFAULT_DESCRIPTION;
+
+  return (
+    <Head>
+      <title>{metaTitle}</title>
+      <meta name="description" content={noDefaultDescription ? description : formattedDescription} />
+      // TODO ページのデフォルトURLが決まったら追記
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:type" content={type} />
+      <meta property="og:description" content={noDefaultDescription ? description : formattedDescription} />
+      <meta property="og:image" content={ogpImageURI} />
+      <meta property="og:site_name" content="abeshi blog" />
+
+      {/* ツイッター ================================================================================================== */}
+      <meta name="twitter:card" content={twitterCardType} />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={noDefaultDescription ? description : formattedDescription} />
+      <meta name="twitter:image" content={ogpImageURI} />
+    </Head>
+  );
+});
