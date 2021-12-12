75a3cc5c1e730a6dab0d6afe85571b2224a6da37
abeshi03 <k_913411@yahoo.co.jp>
2021-12-12 12:19:51 +0900

    ヘッダー修正

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,9 +1,12 @@
 // - フレームワーク =======================================================================================================
 import React, { memo, VFC } from "react";
+import Link from "next/link";
 
 // - アセット ============================================================================================================
 import styles from "./header.module.scss";
+
 // - ルーティング =========================================================================================================
+import { pagesPath } from "../../../lib/$path";
 
 /* eslint-disable-next-line react/display-name */
 export const Header: VFC = memo(() => {
@@ -11,7 +14,9 @@ export const Header: VFC = memo(() => {
     <header className={styles.header}>
       <div className={styles.headerInner}>
         <div className={styles.right}>
-          <div className={styles.logo}>abeshi blog</div>
+          <Link href={pagesPath.$url()}>
+            <a className={styles.logo}>abeshi blog</a>
+          </Link>
         </div>
         <div className={styles.left}>
           <div
