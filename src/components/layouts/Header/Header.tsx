d980606a8a4403813d3efe334f0d0e9e15e031de
abeshi03 <k_913411@yahoo.co.jp>
2021-12-12 14:17:39 +0900

    ヘッダーモーダル: カテゴリーの非同期処理を追加

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,5 +1,5 @@
 // - フレームワーク =======================================================================================================
-import React, { memo, VFC } from "react";
+import React, {memo, useEffect, useState, VFC} from "react";
 import Link from "next/link";
 
 // - アセット ============================================================================================================
@@ -8,8 +8,48 @@ import styles from "./header.module.scss";
 // - ルーティング =========================================================================================================
 import { pagesPath } from "../../../lib/$path";
 
+// - 子コンポーネント =====================================================================================================
+import { Modal } from "../../organisms/Modal/Modal";
+import { CategoriesBadgeFlow } from "../../molecules/CategoriesBadgeFlow/CategoriesBadgeFlow";
+
+// - 型定義 =============================================================================================================
+import { Category } from "../../../types/Category";
+
+// - API ===============================================================================================================
+import { getCategories } from "../../../apis/CategoryAPI";
+
+
 /* eslint-disable-next-line react/display-name */
 export const Header: VFC = memo(() => {
+
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+  const [ categories, setCategories ] = useState<Category[]>([]);
+
+  const onClickModalOpen = () => {
+    setModalIsOpen(true);
+  }
+
+  const onClickCloseModal = (): void => {
+    setModalIsOpen(false);
+  }
+
+  const getBlogCategories = async (): Promise<void> => {
+    try {
+
+      const responseData = await getCategories();
+      setCategories(responseData.contents);
+
+    } catch (error: unknown) {
+
+      console.log("エラーが発生しました")
+    }
+  }
+
+  useEffect(() => {
+    getBlogCategories();
+  }, [])
+
   return (
     <header className={styles.header}>
       <div className={styles.headerInner}>
@@ -22,9 +62,21 @@ export const Header: VFC = memo(() => {
           <div
             className={styles.categoryButton}
             role="button"
+            onClick={onClickModalOpen}
           >カテゴリーで検索</div>
         </div>
       </div>
+
+      <Modal isOpen={modalIsOpen} closeModalFunction={onClickCloseModal}>
+        <div className={styles.headingAndCloseButton}>
+          <h3 className="heading3">カテゴリーで絞り込み</h3>
+          <CategoriesBadgeFlow
+            categories={categories}
+            style={{ marginTop: "20px"}}
+          />
+        </div>
+      </Modal>
+
     </header>
   );
 });
--- src/components/organisms/Modal/Modal.tsx
+++ src/components/organisms/Modal/Modal.tsx
@@ -4,23 +4,28 @@ import React, { memo, VFC } from "react";
 // - アセット ============================================================================================================
 import styles from "./Modal.module.scss";
 
-// - ルーティング =========================================================================================================
 
 type Props = {
   children: React.ReactNode;
   isOpen: boolean;
   modalInnerColor?: string;
+  closeModalFunction: () => void;
 }
 
 /* eslint-disable-next-line react/display-name */
 export const Modal: VFC<Props> = memo((props) => {
-  const { children, isOpen, modalInnerColor = "white" } = props;
+
+  const { children, isOpen, modalInnerColor = "white", closeModalFunction } = props;
 
   return (
     <>
       { isOpen &&
         <div className={styles.modal}>
           <div className={styles.modalInner} style={{background: modalInnerColor}}>
+            <div
+              className={styles.closeButton}
+              onClick={closeModalFunction}
+            >×</div>
             { children }
           </div>
         </div>
