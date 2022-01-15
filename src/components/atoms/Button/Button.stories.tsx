6129a9d5e3f6a138b003d20ade11149b114b8666
abeshi03 <k_913411@yahoo.co.jp>
2022-01-15 17:20:50 +0900

    storybook: ボタンコンポーネント修正

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -9,4 +9,18 @@ export default {
   component: Button
 } as ComponentMeta<typeof Button>;
 
-export const SkyBlue: ComponentStory<typeof Button> = () => <Button color="SKY_BLUE" size="BIG" path="#">青くて大きい</Button>
+export const SkyBlue__Big: ComponentStory<typeof Button> = () => (
+  <Button color="SKY_BLUE" size="BIG" path="#">青くて大きい</Button>
+);
+
+export const SkyBlue__Small: ComponentStory<typeof Button> = () => (
+  <Button color="SKY_BLUE" size="SMALL" path="#">青くて小さい</Button>
+);
+
+export const White__Big: ComponentStory<typeof Button> = () => (
+  <Button color="WHITE" size="BIG" path="#">白くて大きい</Button>
+);
+
+export const White__Small: ComponentStory<typeof Button> = () => (
+  <Button color="WHITE" size="SMALL" path="#">白くて小さい</Button>
+);
